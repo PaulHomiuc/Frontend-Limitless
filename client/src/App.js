efeacc6ac5bd1a7b92acaad3e7b1ee5e11115389
PaulHomiuc <paul.homiuc@student.usv.ro>
2022-03-03 00:14:44 +0200

    v0.1

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import "./App.css";
+import Dashboard from "./dashboard/Dashboard.js";
+import Preferences from "./preferences/Preferences.js";
+import Login from "./Login/Login.js";
+
+const App = () => {
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+
+          <Route path="/preferences" element={<Preferences />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+export default App;
