fc2327b31065b20c4b0f2644332a3333e2e5ef29
PaulHomiuc <paul.homiuc@student.usv.ro>
2022-03-04 16:51:38 +0200

    v0.5

--- client/src/Login/Login.js
+++ client/src/Login/Login.js
@@ -1,22 +1,51 @@
 import React, {Component} from "react";
 import "./Login.css";
-
+import { useState } from "react";
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password,setPassword]=useState("");
+  const [message, setMessage] = useState("");
+
+  const emailValidation = () => {
+    const regEx = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g;
+    if (regEx.test(email)) {
+      setMessage("Email is Valid");
+    } else if (!regEx.test(email) && email !== "") {
+      setMessage("Email is Not Valid");
+    } else {
+      setMessage("");
+    }
+    if(password.length<8)
+    setMessage("The password should have at least 8 characters");
+  
+  };
+ 
+  const handleOnChange = (e) => {
+    setEmail(e.target.value);
+    
+};
   return (
    
     <div className="Login">
       <h1>Please Log In</h1>
       <form>
         <label>
-          <p>Username</p>
-          <input type="text" />
+          <p>E-mail address</p>
+          <input  id="email"
+          className="input"
+          type="email"
+          placeholder="email"
+          value={email}
+          required
+          onChange={handleOnChange} />
         </label>
         <label>
           <p>Password</p>
-          <input type="password" />
+          <input id="password" type="password"  
+           placeholder="password"  required    onChange={handleOnChange}  isPassword/>
         </label>
         <div>
-          <button type="submit">Submit</button>
+          <button type="submit" onClick={emailValidation}>Submit</button>
         </div>
       </form>
     </div>
