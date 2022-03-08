c8cee1e2c84e162c1173de070eb62d0fd5f04ff4
PaulHomiuc <paul.homiuc@student.usv.ro>
2022-03-09 00:17:42 +0200

    Employee desk request form added

--- client/src/employee/Employee.js
+++ client/src/employee/Employee.js
@@ -2,19 +2,42 @@ import React, {Component} from "react";
 import "./Employee.css";
 import { useState } from "react";
 export default function Employee() {
-
+    function FormToggle(){
+     var box1 = document.getElementById("FormRequest");
+        if(box1.style.display=="none")
+        {
+          box1.style.display="block";
+        }
+        else
+        {
+          box1.style.display="none";
+        }
+    }
     return (
-   
+        <body>
+           <div className="Account">
+          <label>Your email here</label>
+          <img className="roundedImg" src="user.png" alt="UserIcon" ></img>
+          <button className="btn">Logout</button>
+          </div>
         <div className="Dashboard">
-          <label>You are logged in as Employee</label>
+          <label className="buttonMenu">You are logged in as Employee</label>
           <div className="element1">
            <img  className ="Information" src="/Information.png" ></img>
            <button className="buttonMenu" >Account information</button>
            </div>
            <div className="element2">
            <img className ="Information" src="writePage.png" ></img>
-          <button className="buttonMenu" >Desk request</button>
+          <button className="buttonMenu" id="btnRequest" onClick={FormToggle} >Desk request</button>
           </div>
         </div>
+        <div className="DeskRequest" id="FormRequest" >
+          <h1>File a desk Request</h1>
+          <h3>Select an Office</h3>
+          <h3 >  Reason for request</h3>
+        <textarea className="Reason"></textarea>
+        <button className="btn">Submit request</button>
+        </div>
+          </body>
       )
 }
