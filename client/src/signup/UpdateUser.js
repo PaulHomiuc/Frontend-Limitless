f974483555b196066b64a7357861d28d5e32ea0b
claudiu-reut <claudiu.reut@student.usv.ro>
2022-03-15 01:32:39 +0200

    fixed user update page

--- client/src/signup/UpdateUser.js
+++ client/src/signup/UpdateUser.js
@@ -4,37 +4,65 @@ import React, {useState, useEffect} from "react";
 
 import {useParams, useNavigate} from "react-router-dom";
 
-export const updateUser = (user, id) => {
-  console.log(user);
-  fetch(`http://localhost:4000/update/${id}`, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(user),
-  });
-};
 const regEx = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g;
 export const getUser = (id) => fetch(`http://localhost:4000/users/${id}`).then((res) => res.json());
 export const UpdateUser = () => {
   const match = useParams();
   const [user, setUser] = useState("");
   const history = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [role, setRole] = useState("");
+  const [gender, setGender] = useState("");
+  const [date, setDate] = useState("");
+  const [national, setNational] = useState("");
+  const updateUser = (id) => {
+    fetch(`http://localhost:4000/update/${id}`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        fname,
+        lname,
+        email,
+        password,
+        role,
+        gender,
+        date,
+        national,
+        id,
+      }),
+    });
+  };
   useEffect(() => {
     const fetchTodo = async () => {
       const user = await getUser(match.id);
       setUser(user);
-      const date = user.birthDate;
-      var str = date.substring(0, 10);
+      setEmail(user.email);
+      setFname(user.firstName);
+      setLname(user.lastName);
+      setPassword(user.password);
+      setRole(user.role);
+      setGender(user.gender);
+      setDate(user.birthDate);
+      setNational(user.nationality);
 
+      const datetime = user.birthDate;
+
+      var str = datetime.substring(0, 10);
+      setDate(str);
       document.getElementById("date").value = str;
     };
     fetchTodo();
   }, []);
 
-  async function onSubmit(data) {
-    await updateUser(data, match);
+  async function onSubmit(d) {
+    await updateUser(match);
 
     history.push("/");
   }
@@ -52,12 +80,12 @@ export const UpdateUser = () => {
               </p>
 
               <input
-                defaultValue={user.firstName}
+                defaultValue={fname}
                 id="fname"
                 type="text"
                 placeholder="First Name"
                 required
-                onChange={(e) => (user.firstName = e.target.value)}
+                onChange={(e) => setFname(e.target.value)}
               />
             </label>
             <label>
@@ -65,12 +93,12 @@ export const UpdateUser = () => {
                 Last Name<b>*</b>
               </p>
               <input
-                defaultValue={user.lastName}
+                defaultValue={lname}
                 id="lname"
                 type="text"
                 placeholder="Last Name"
                 required
-                onChange={(e) => (user.lastName = e.target.value)}
+                onChange={(e) => setLname(e.target.value)}
               />
             </label>
             <label>
@@ -83,9 +111,9 @@ export const UpdateUser = () => {
                 className="input"
                 type="email"
                 placeholder="email"
-                defaultValue={user.email}
+                defaultValue={email}
                 required
-                onChange={(e) => (user.email = e.target.value)}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </label>
             <label>
@@ -98,8 +126,8 @@ export const UpdateUser = () => {
                 placeholder="password"
                 required
                 isPassword
-                defaultValue={user.password}
-                onChange={(e) => (user.password = e.target.value)}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </label>
             <label>
@@ -111,8 +139,8 @@ export const UpdateUser = () => {
                 id="roles"
                 required
                 placeholder="admin"
-                defaultValue={user.role}
-                onChange={(e) => (user.role = e.target.value)}
+                defaultValue={role}
+                onChange={(e) => setRole(e.target.value)}
               >
                 {" "}
                 <option selected disabled>
@@ -132,8 +160,8 @@ export const UpdateUser = () => {
                 id="genders"
                 required
                 placeholder="Male"
-                defaultValue={user.gender}
-                onChange={(e) => (user.gender = e.target.value)}
+                defaultValue={gender}
+                onChange={(e) => setGender(e.target.value)}
               >
                 {" "}
                 <option selected disabled>
@@ -147,12 +175,7 @@ export const UpdateUser = () => {
 
             <label>
               <p>Date of birth</p>
-              <input
-                type="date"
-                name="date"
-                id="date"
-                onChange={(e) => (user.birthDate = e.target.value)}
-              />
+              <input type="date" name="date" id="date" onChange={(e) => setDate(e.target.value)} />
             </label>
             <label>
               <p>Nationality</p>
@@ -161,13 +184,13 @@ export const UpdateUser = () => {
                 placeholder="Nationality"
                 id="nationality"
                 name="nationality"
-                defaultValue={user.nationality}
-                onChange={(e) => (user.nationality = e.target.value)}
+                value={national}
+                onChange={(e) => setNational(e.target.value)}
               />
             </label>
 
             <div>
-              <button className="buttonOffice" type="submit" onClick={onSubmit()}>
+              <button className="buttonOffice" type="submit" onClick={onSubmit}>
                 Save
               </button>
             </div>
